9315a57f2ba5000b2fe8ec3bdc390f0793e5ef1b
Washington Trucido <wjtrucido@gmail.com>
2023-02-12 15:14:58 -0300

    was implemented controllerGetPublicationbyString.ts

---
+++ Backend/controllers/controllerGetPublicationbyString.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import Publication from "../models/publication";
+
+export const getPublicationByString = (req: Request, res: Response) => {
+    const { string } = req.params
+    interface TopicDoc {
+        Topic: String,
+        NumDoc: String
+    }
+    const docNumTopic: Array<TopicDoc> = []
+    Publication.find({ Topic: { $regex: `^${string}`, $options: 'i' } })
+        .then(docs => {
+            docs.map(doc => docNumTopic.push({ Topic: doc.Topic, NumDoc: doc.num_Publication }))
+            res.json(docNumTopic)
+        })
+        .catch(err => console.log(err))
+}
--- Backend/routes/RouterPublication.ts
+++ Backend/routes/RouterPublication.ts
@@ -5,10 +5,12 @@ import { editPublication } from "../controllers/controllerEditPublication";
 import { home } from "../controllers/controllerHome";
 import { checking } from "../middlewares/checking";
 import { notFoundRoute } from "../controllers/controllerNotFoundRoute";
+import { getPublicationByString } from "../controllers/controllerGetPublicationbyString";
 
 const router = express.Router()
 
 router.get("/getPublications", getPublications)
+router.get("/getPublication/:string", getPublicationByString)
 router.post("/createPublication", checking, createPublication)
 router.put("/editPublication/:id", checking, editPublication)
 router.get("", home)

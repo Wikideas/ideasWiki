9416d041376b1ce042fe9893945ba6e7070c44b5
Lihuel <muracciole.al@gmail.com>
2023-03-24 18:42:59 -0300

    Navbar

---
+++ frontend/src/pages/SobreNosotros.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SobreNosotros() {
+  return (
+    <div className='sobreNosotros'>
+      <h1>Sobre Nosotros</h1>
+    </div>
+  );
+}
+
+export default SobreNosotros;

947bca432ce1a6d4b1241359b9831d5e04d09e30
Washington Trucido <wjtrucido@gmail.com>
2023-05-20 13:46:57 -0500

    createPublication and getLastPublication fix bug

--- Backend/src/utils/countPublications.ts
+++ Backend/src/utils/countPublications.ts
@@ -1,12 +1,12 @@
 import Publication from '../models/publication.model';
 
-export const numb = async () => {
+export const numberPublications = async () => {
     return new Promise(resolve => {
         Publication.countDocuments({}, (err: Error, count: Number) => {
             if (err) {
                 console.error(err);
             } else {
-                resolve(count.toString())
+                resolve(count)
             }
         });
     })

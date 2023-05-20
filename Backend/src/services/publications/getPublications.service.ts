1df110cce3e656457a3f8f06384e0ebc0c536808
Washington Trucido <wjtrucido@gmail.com>
2023-05-20 09:19:51 -0500

    changed root to wikideas

--- Backend/src/services/publications/getPublications.service.ts
+++ Backend/src/services/publications/getPublications.service.ts
@@ -1,9 +1,16 @@
 import Publication from '../../models/publication.model';
 
-export const getPublicationsService = async () => {
+export const getPublicationsService = async (usePagination: any, from: any, limit: any) => {
     try {
-        const publications = await Publication.find();
-        return publications;
+        if (usePagination === 'true') {
+            const publications = await Publication.find()
+                .skip(Number(from))
+                .limit(Number(limit));
+            return publications;
+        } else {
+            const publications = await Publication.find();
+            return publications;
+        }
     } catch (error) {
         throw new Error('An error occurred while trying to get the publications.');
     }

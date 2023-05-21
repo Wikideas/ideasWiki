d1ecd5ae76dd5bbe9d6e4ca60713669738d69a6c
Washington Trucido <wjtrucido@gmail.com>
2023-05-21 12:42:09 -0500

    getSuggestedPublications bug fix, pagination implementation

--- Backend/src/controllers/publications/getPublications.controller.ts
+++ Backend/src/controllers/publications/getPublications.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import { getPublicationsService } from '../../services/publications/getPublications.service';
+import { numberPublications } from '../../utils/countPublications';
 
 export const getPublicationsController = async (req: Request, res: Response) => {
     const { usePagination = 'false', from = '0', limit = '5' } = req.query;
     try {
-        const publications = await getPublicationsService(usePagination, from, limit);
-        res.status(200).json({ publications })
+        const [[publications, numberOfPublicationsRendered], totalPublications] = await Promise.all([
+            getPublicationsService(
+                usePagination.toString(),
+                from.toString(),
+                limit.toString()
+            ),
+            numberPublications()
+        ]);
+        res.status(200).json({
+            numberOfPublicationsRendered: numberOfPublicationsRendered,
+            totalPublications: totalPublications,
+            publications: publications
+        })
     } catch (error: any) {
         res.status(500).json({ error: `${error.message}` });
     }

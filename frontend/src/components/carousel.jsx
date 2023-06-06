a4cd60116f65490197a46bbd3256007a304b24e4
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-06-06 00:10:20 -0500

    searchBar

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -12,9 +12,9 @@ const Carousel = () => {
   //console.log(category);
 
  
-  const { loading, data: category } = useApiCategory(`https://serviceone.onrender.com/apiWikiIdeasV1d/getCategory`)
-  console.log(loading)
-  console.log(category)
+  const { loading, data: category } = useApiCategory(`https://serviceone.onrender.com/api-wikideas/categories`)
+ /*  console.log(loading)
+  console.log(category) */
 
   //Medidas para el carousel
     const [width, setWidth] = useState({right:0, left:-770});
